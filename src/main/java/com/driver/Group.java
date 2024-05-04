1abf7fc2d59ace4787982419c5e5b7d7d08883bd
Ritik Vishwakarma <ritikvishwakarma739@gmail.com>
2024-05-04 19:20:42 +0530

    second

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -1,7 +1,44 @@
 package com.driver;
 
+import org.apache.catalina.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Group {
     private String name;
-    private int numberOfParticipants;
+    private List<User> participants;
 
+    public Group(String name, int size) {
+        this.name = name;
+        this.participants = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<User> participants) {
+        this.participants = participants;
+    }
+
+    public void addParticipant(User user) {
+        participants.add(user);
+    }
+
+    public void removeParticipant(User user) {
+        participants.remove(user);
+    }
+
+    public int getNumberOfParticipants() {
+        return participants.size();
+    }
 }
